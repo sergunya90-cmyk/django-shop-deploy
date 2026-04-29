fcb6d30496444b74d1d969af129b13fc00f92156
user <popov.sergey.info.gold@yansex.kz>
2026-04-29 18:16:07 +0500

    feat: добавлены ModelForm для создания продуктов и заказов

--- Forms/mysite/shopapp/views.py
+++ Forms/mysite/shopapp/views.py
@@ -2,9 +2,11 @@ from timeit import default_timer
 
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .models import Product, Order
+from .forms import ProductForm, OrderForm
 
 
 def shop_index(request: HttpRequest):
@@ -39,3 +41,31 @@ def orders_list(request: HttpRequest):
         "orders": Order.objects.select_related("user").prefetch_related("products").all(),
     }
     return render(request, 'shopapp/orders-list.html', context=context)
+
+
+def create_product(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse("shopapp:products_list")
+            return redirect(url)
+    else:
+        form = ProductForm()
+
+    context = {"form": form}
+    return render(request, "shopapp/create-product.html", context=context)
+
+
+def create_order(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse("shopapp:orders_list")
+            return redirect(url)
+    else:
+        form = OrderForm()
+
+    context = {"form": form}
+    return render(request, "shopapp/create-order.html", context=context)
