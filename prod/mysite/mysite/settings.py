ae808b912818f5b4a539fc2418ca0067f9c57d60
user <popov.sergey.info.gold@yansex.kz>
2026-06-08 20:05:05 +0500

    gjgyjf

--- prod/mysite/mysite/settings.py
+++ prod/mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hvxn%qq=gyw^4*o2lo1#bw0=wh#ux9s8h!=@c608arf_gz3+^7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "147.45.215.200",
