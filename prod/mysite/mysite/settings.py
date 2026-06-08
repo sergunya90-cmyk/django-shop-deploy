dafaf26c96450cfdea4ededee7678df5b769d4c9
user <popov.sergey.info.gold@yansex.kz>
2026-06-08 18:57:53 +0500

    изменения на сервер MIDDLEWARE

--- prod/mysite/mysite/settings.py
+++ prod/mysite/mysite/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # "django.middleware.cache.UpdateCacheMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'  
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
