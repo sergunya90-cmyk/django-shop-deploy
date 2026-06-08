32c54f342093674dce1b0f9eeb4e9f3de592e864
user <popov.sergey.info.gold@yansex.kz>
2026-06-08 20:42:04 +0500

    DEBUG = False

--- prod/mysite/mysite/settings.py
+++ prod/mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-hvxn%qq=gyw^4*o2lo1#bw0=wh#ux9s8h!=@c608arf_gz3+^7'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = [
